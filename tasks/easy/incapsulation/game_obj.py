6d054a6c96e4bb1b090261396ee4f133272464a6
yungun <kirill.dyuzhij@gmail.com>
2021-07-13 11:09:44 +0300

    new solves
    *book_card
    *game_obj
    *laptop_game

--- tasks/easy/incapsulation/game_obj.py
+++ tasks/easy/incapsulation/game_obj.py
@@ -15,11 +15,27 @@ class GameObject:
     x: int
     y: int
 
-    @property
-    def move(self):
-        return self.x, self.y
-
-    @move.setter
-    def move(self, x, y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def move(self, delta_x, delta_y):
+        self.x = delta_x
+        self.y = delta_y
+
+
+    @property
+    def delta_x(self):
+        return self.x
+
+    @delta_x.setter
+    def delta_x(self, delta_x):
+        self.x = delta_x
+
+    @property
+    def delta_y(self):
+        return self.y
+
+    @delta_y.setter
+    def delta_y(self, delta_y):
+        self.y = delta_y
--- tasks/easy/incapsulation/laptop_game.py
+++ tasks/easy/incapsulation/laptop_game.py
@@ -15,6 +15,29 @@
 
 
 class Laptop:
-    cpu_cores: int
-    gpu_total: int
-    ram_total
+    __cpu_cores: int
+    __gpu_total: int
+    __ram_total: int
+
+    def __init__(self, cpu_cores, gpu_total, ram_total):
+        self.__cpu_cores = cpu_cores
+        self.__gpu_total = gpu_total
+        self.__ram_total = ram_total
+
+    @property
+    def cpu_cores(self):
+        return self.__cpu_cores
+
+    @property
+    def gpu_total(self):
+        return self.__gpu_total
+
+    @property
+    def ram_total(self):
+        return self.__ram_total
+
+    def can_play(self, game_name, cpu_cores, gpu_total, ram_total):
+        if self.cpu_cores > cpu_cores and self.__gpu_total > gpu_total and self.__ram_total > ram_total:
+            print(f'На данном ПК есть возможность играть в {game_name}')
+        else:
+            print(f'На данном ПК невозможно играть в {game_name}')
