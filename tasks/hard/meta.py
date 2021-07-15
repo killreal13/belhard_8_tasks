356a2f2acb41e9e1db8dcdd0f43daa7df41ed99b
yungun <kirill.dyuzhij@gmail.com>
2021-07-15 20:56:04 +0300

    new solve
    *meta

--- tasks/hard/meta.py
+++ tasks/hard/meta.py
@@ -6,20 +6,19 @@ ValueError
 
 
 def attribute_checker(method):
-    def wrapper(*args, **kwargs):
-        if method.__name__ == '__setattr__':
-            if
-        method(*args, **kwargs)
+    def wrapper(self, name, values):
+        attribute_annotation = self.__annotations__.get(name)
+        if type(values) != attribute_annotation:
+            raise ValueError
+        return method
     return wrapper
 
 
 class AttributeTypeMeta(type):
+
     def __new__(mcs, name, bases, attr):
         new_class = super().__new__(mcs, name, bases, attr)
-        attr_dict = {k: v for k, v in mcs.__dict__.items() if callable(v)}
-        for k, v in attr_dict.items():
-            new_value = attribute_checker(v)
-            setattr(new_class, k, new_value)
+        new_class.__setattr__ = attribute_checker(new_class.__setattr__)
         return new_class
 
 
@@ -34,5 +33,4 @@ class Strings(metaclass=AttributeTypeMeta):
     def print_values(self, a):
         print(self.values, a)
 
-aasd = Strings('44', 123)
-aasd.print_values(12)
+aasd = Strings('123', 123)
