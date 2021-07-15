14b3d61c6c0eca93d1242323981d5ce9a08cee39
yungun <kirill.dyuzhij@gmail.com>
2021-07-15 11:24:18 +0300

    new solves
    *game
    *main

--- tasks/hard/game.py
+++ tasks/hard/game.py
@@ -22,3 +22,50 @@
 Программа завершается, когда останется один юнит, на экран выводится сообщение о том,
 кто одержал победу.
 """
+import random
+
+
+class Warrior:
+    name: str
+    health_points: int
+
+    def __init__(self, name):
+        self.name = name
+        self.health_points = 100
+
+    def hit(self, other_warrior):
+            other_warrior.health_points -= 20
+            print(f'{self.name} attacked {other_warrior.name} and left him {other_warrior.health_points} health points')
+
+
+class Arena:
+    warrior_list: list
+
+    def __init__(self, warriors_list):
+        self.warrior_list = warriors_list
+
+    def fight(self):
+        while True:
+            for i in range(len(self.warrior_list)):
+                attacking_warrior = random.choice(self.warrior_list)
+                defending_warrior = random.choice(self.warrior_list)
+                if attacking_warrior.name == defending_warrior.name:
+                    continue
+                else:
+                    Warrior.hit(attacking_warrior, defending_warrior)
+                if defending_warrior.health_points == 0:
+                    self.warrior_list.remove(defending_warrior)
+            if len(self.warrior_list) == 1:
+                print(f'{self.warrior_list[0].name} wins!')
+                break
+
+
+warrior_1 = Warrior('Deiveson Figueiredo')
+warrior_2 = Warrior('Dustin Poirier')
+warrior_3 = Warrior('Edson Barboza')
+warrior_4 = Warrior('Charles Oliveira')
+warrior_5 = Warrior('Max Holloway')
+
+warrior_list = [warrior_1, warrior_2, warrior_3, warrior_4, warrior_5]
+arena_1 = Arena(warrior_list)
+arena_1.fight()
--- tasks/medium/realt/main.py
+++ tasks/medium/realt/main.py
@@ -2,7 +2,6 @@ from house import House
 from townhouse import Townhouse
 from person import Person
 
-
 if __name__ == '__main__':
     house_1 = House('Abbey Road', 32, 50000)
     house_2 = House('Piccadilly', 47, 87000)
@@ -12,9 +11,9 @@ if __name__ == '__main__':
     person_1.make_deal(house_4)
     person_1.earn_money(36000)
     person_1.make_deal(house_4)
-    person_1.earn_money(100000) #получил наследство от бабушки из Бастилии
+    person_1.earn_money(100000)  # получил наследство от бабушки из Бастилии
     person_1.make_deal(house_4)
-    person_1.earn_money(500000) #выиграл в казино
-    person_1.make_deal(house_1) #процесс становления хозяином жизни
+    person_1.earn_money(500000)  # выиграл в казино
+    person_1.make_deal(house_1)  # процесс становления хозяином жизни
     person_1.make_deal(house_3)
-    person_1.make_deal(house_2) #стал хозяином жизни
+    person_1.make_deal(house_2)  # стал хозяином жизни
