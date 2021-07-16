8ab96ea79f6e872f27368d8a7efbc1a65a5e899d
yungun <kirill.dyuzhij@gmail.com>
2021-07-16 08:52:21 +0300

    new solves
    *meta_attr

--- tasks/hard/meta_attr.py
+++ tasks/hard/meta_attr.py
@@ -13,22 +13,28 @@ File "<stdin>", line 20, in <module>
 
 Сделать возможным данный механизм
 """
-def getattr_1(method):
-    def wrapper(self, name, value):
-        print(name)
-    return wrapper
 
+
+def getattr_1(method, cls):
+    def wrapper(self, *args, **kwargs):
+        print(kwargs)
+        new_dict = {k: v for k, v in kwargs.items()}
+        for k, v in new_dict.items():
+            setattr(cls, k, v)
+        return method(cls)
+    return wrapper
 
 
 class AttributeMeta(type):
     def __new__(mcs, name, bases, attr):
         new_class = super().__new__(mcs, name, bases, attr)
-        new_class.__setattr__ = getattr_1(new_class.__setattr__)
-        print(new_class.__setattr__)
+        mcs.__call__ = getattr_1(mcs.__call__, new_class)
         return new_class
 
 
 class Man(metaclass=AttributeMeta):
     pass
 
-me = Man(qwe = 80, qr = 100)
+
+me = Man(qwe=80, qr=100)
+print(me.qr)
